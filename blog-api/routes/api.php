67be250c6ea680c1c4dc72cbd48e9bdcc3abe301
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-16 15:16:41 +0500

    initial change in branch SalmanIrfan

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// change by salman irfan
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
