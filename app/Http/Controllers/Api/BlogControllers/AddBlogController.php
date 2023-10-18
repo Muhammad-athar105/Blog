65c6a77a3d1294315158a0a6ba9ee6e1f2b96fc5
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-18 11:24:05 +0500

    add blod controller initialized

---
+++ app/Http/Controllers/Api/BlogControllers/AddBlogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\BlogControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddBlogController extends Controller
+{
+    // add a new blog function
+    
+}
